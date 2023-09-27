d7585d1a014b23cb3b04cbee8b3f047faa8d1ffe
Mk.2 <55669745+mk2-mc@users.noreply.github.com>
2023-09-27 20:44:59 +0100

    Update

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const button = document.querySelector(".toggle-button");
+const toggle = document.querySelector(".toggle-switch");
+const label = document.querySelector(".toggle-label");
 const price = document.querySelectorAll(".card-price");
+let annual = false;
 
 button.addEventListener("click", () => {
-  const state = button.getAttribute("aria-pressed");
-  button.setAttribute("aria-pressed", state === "false");
+  if (!annual) {
+    label.innerHTML = "Annual pricing enabled";
+  } else {
+    label.innerHTML = "Monthly pricing enabled";
+  }
   
+  annual = !annual;
+  toggle.classList.toggle("toggle-left");
+
   for (let i = 0; i < price.length; i++) {
     price[i].classList.toggle("hidden");
   }
